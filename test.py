c136e35334d3b17cec16dccd651062234b465c2e
Matthew Downes <mddownes@umass.edu>
2015-01-27 22:45:32 -0500

    m

--- test.py
+++ test.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python3
-print("Hello World")
+import sys
+
+print("%s"%(sys.argv[1]))
+
+file = open(sys.argv[1])
+
+lineCount = 0
+charCount = 0
+print ("%08x"%lineCount, end="  ")
+while True:
+ currentChar = file.read(1)
+ if not currentChar:
+  break
+ charCount = charCount + 1
+ if charCount == 8:
+  print ("%02x"%ord(currentChar), end ="  ")
+ elif charCount == 16:
+  lineCount = lineCount + 1
+  print ("%02x"%ord(currentChar))
+  print ("%07x%x"%(lineCount,0), end="  ")
+  charCount = 0;
+  
+ else:
+  print ("%02x"%ord(currentChar), end =" ")
+ 
