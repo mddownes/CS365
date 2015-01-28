232d7cdbe4b85e07d2e35bba62fba53ef327601c
Matthew Downes <mddownes@umass.edu>
2015-01-28 09:55:28 -0500

    g

--- test.py
+++ test.py
@@ -1,26 +1,51 @@
 #!/usr/bin/env python3
 import sys
 
-print("%s"%(sys.argv[1]))
 
-file = open(sys.argv[1])
 
-lineCount = 0
-charCount = 0
-print ("%08x"%lineCount, end="  ")
-while True:
- currentChar = file.read(1)
- if not currentChar:
-  break
- charCount = charCount + 1
- if charCount == 8:
-  print ("%02x"%ord(currentChar), end ="  ")
- elif charCount == 16:
-  lineCount = lineCount + 1
-  print ("%02x"%ord(currentChar))
-  print ("%07x%x"%(lineCount,0), end="  ")
-  charCount = 0;
-  
- else:
-  print ("%02x"%ord(currentChar), end =" ")
- 
+def hexdump(fileName):
+
+
+ file = open(fileName)
+ lineCount = -1
+ charList = []
+
+
+ while True:
+  currentChar = file.read(1)
+  if not currentChar:
+   break
+  if ord(currentChar) not in range(32,126):
+   charList.append(".")
+  else:
+   charList.append(currentChar)
+
+  if len(charList) == 16:
+   lineCount = lineCount + 1
+   printLineNumber(lineCount)
+   printHex(charList)
+   printAscii(charList)
+   charList = []
+
+
+
+
+def printLineNumber(number):
+	print ("%07x%x"%(number,0), end="  ")
+
+def printHex(list):
+	for x in range(0,16):
+	 if x == 7 or x == 15:
+		 print ("%02x"%ord(list[x]), end ="  ")
+	 else:
+		 print ("%02x"%ord(list[x]), end =" ")
+
+
+def printAscii(asciiList):
+ print ("%s"%"|", end = "")
+ for x in asciiList:
+  print ("%s"%x, end = "")
+ print ("%s"%"|")
+
+if __name__ == "__main__":
+	hexdump(sys.argv[1])
